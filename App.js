86f751cdc4b1988e9c2cf7c10cc19b3f1369486e
Ruan Lucas <79365912+RuanLucasGD@users.noreply.github.com>
2022-03-31 10:49:38 -0300

    improv: simple view

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native-web';
+
+function requestUserRepository(username, onGetRequestData) {
+
+  const request = new XMLHttpRequest();
+  const githubUrl = `https://api.github.com/users/${username}`;
+
+  request.open('GET', githubUrl, true);
+
+  request.onload = () => {
+
+    const data = JSON.parse(request.response);
+    onGetRequestData(data);
+  }
+
+  request.send();
+}
 
 export default function App() {
+
+  const drawList = (dt) => {
+    <FlatList
+      data={[dt]}
+      renderItem={(item) => console.log("bla")}
+    />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      {
+        requestUserRepository("RuanLucasGD", (data) => { drawList(data) })
+      }
+    </View>);
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +42,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item: {
+    fontSize: 10,
+    color: "black"
+  }
 });
