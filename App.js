c2c789797217c8f98a137401bfa22af91edadc88
Ruan Lucas <79365912+RuanLucasGD@users.noreply.github.com>
2022-04-04 18:39:42 -0300

    improv: add project

--- App.js
+++ App.js
@@ -1,49 +1,137 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { FlatList } from 'react-native-web';
+import { Button, FlatList } from 'react-native-web';
+import { Image } from 'react-native'
+import { useEffect } from 'react'
 
-function requestUserRepository(username, onGetRequestData) {
+function requestApiData(api, onGetRequestData) {
 
   const request = new XMLHttpRequest();
-  const githubUrl = `https://api.github.com/users/${username}`;
 
-  request.open('GET', githubUrl, true);
+  request.open('GET', api, true);
 
   request.onload = () => {
 
     const data = JSON.parse(request.response);
-    onGetRequestData(data);
+    if (onGetRequestData) {
+      onGetRequestData(data);
+    }
   }
 
   request.send();
 }
 
+function openPerfil() {
+  console.log("bla")
+}
+
 export default function App() {
 
-  const drawList = (dt) => {
-    <FlatList
-      data={[dt]}
-      renderItem={(item) => console.log("bla")}
-    />
+  const perfilApi = `https://api.github.com/users/RuanLucasGD`;
+  const repoApi = `https://api.github.com/users/RuanLucasGD/repos`;
+
+  var emptyWindow = (
+    <View style={styles.container}>
+      <Text style={styles.item}> bla </Text>
+    </View>
+  );
+
+  const [appWindow, setAppWindow] = useState(emptyWindow);
+  const [userDataFinded, setUserDataFinded] = useState(false);
+  const [repoDataFinded, setRepoDataFinded] = useState(false);
+  let userData = {};
+  let repoData = {};
+  const [allDataFinded, setAllDataFinded] = useState(false)
+
+  const checkIfAllDataIsFinded = () => { return userDataFinded && repoDataFinded; }
+
+  const drawFullWindow = () => {
+
+    setAppWindow((
+      < View style={styles.container} >
+        <View style={styles.Center}>
+          <Image style={styles.AvatarImg} source={{ uri: userData.avatar_url }} />
+          <Text style={styles.Name}> {userData.name} </Text>
+          <Text style={styles.Bio}> {userData.bio} </Text>
+          <Text>Hello</Text>
+        </View>
+        <View style={styles.container}>
+        </View>
+        <Button title="Open perfil" onPress={openPerfil} color="#ee9b00"></Button>
+      </View >
+    ))
   }
 
-  return (
-    <View style={styles.container}>
-      {
-        requestUserRepository("RuanLucasGD", (data) => { drawList(data) })
+  requestApiData(perfilApi, (data) => {
+
+    if (!userDataFinded) {
+      userData = data;
+      setUserDataFinded(true)
+    }
+  })
+
+  requestApiData(repoApi, (data) => {
+
+    if (!repoDataFinded) {
+      setRepoDataFinded(true)
+      repoData = data;
+    }
+  })
+
+
+  useEffect(() => {
+
+    if (!allDataFinded) {
+
+
+      if (checkIfAllDataIsFinded()) {
+        drawFullWindow()
+        setAllDataFinded(true)
+        console.log(repoData) 
       }
-    </View>);
+    }
+  })
+
+  return appWindow;
 }
 
 const styles = StyleSheet.create({
+
   container: {
+    padding: '5%',
+    display: 'flex',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#0c1e2b',
+    alignItems: 'center'
   },
-  item: {
-    fontSize: 10,
-    color: "black"
+  AvatarImg: {
+    paddingBottom: 100,
+    width: 200,
+    height: 200,
+    borderRadius: 200 / 2
+  },
+  Name: {
+    fontSize: 50,
+    color: "#fff",
+    fontWeight: 'bold'
+  },
+  Bio: {
+    fontSize: 20,
+    color: "#fff",
+    fontStyle: 'italic'
+  },
+  Button: {
+    heiht: 50,
+    width: 200,
+    justifyContent: 'center',
+    display: 'flex',
+    alignContent: 'center',
+    margin: 20
+  },
+  Center: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    alignItems: 'center'
   }
 });
